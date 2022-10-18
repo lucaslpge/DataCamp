dba3883c3cc49a02ade5aef7c0510da147640e8a
lucaslpge <73127231+lucaslpge@users.noreply.github.com>
2022-10-18 15:03:33 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import altair as alt
+from streamlit_option_menu import option_menu
+
+
+
+st.markdown("#Home")
+st.sidebar.markdown("Home")
+
+def home():
+    st.markdown("#Home")
+    st.sidebar.markdown("#Home")
+
+def page2():
+    st.markdown("#Trafic")
+    st.sidebar.markdown("#Trafic")
+
+def page3():
+    st.markdown("#Route Planning")
+    st.sidebar.markdown("#Route Planning")
+
