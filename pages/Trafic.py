672de5afbc7733153cb7f065d7cb51d9c67d0f99
lucaslpge <lucas.le.page@efrei.net>
2022-10-18 16:23:15 +0200

    Update Trafic.py

--- pages/Trafic.py
+++ pages/Trafic.py
@@ -27,5 +27,7 @@ start_minute, end_minute = st.select_slider(
     'Minute',
     options=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 59],
     value=(15, 30))
-filtered_data = df.loc[(df.hour == hour_to_filter) & (df.day == day_to_filter) & (df.minute > start_minute) & (df.minute < end_minute)]
+
+st.write('You are looking at the trafic at the ', day_to_filter, '/02/2008 between', hour_to_filter, ':', start_minute, 'and', hour_to_filter, ':', end_minute)
+filtered_data = df.loc[(df.hour == hour_to_filter) & (df.day == day_to_filter) & (df.minute >= start_minute) & (df.minute <= end_minute)]
 st.map(filtered_data)
