21ef92e23e1322867eeec99fe24eaac6aa6b5cde
lucaslpge <73127231+lucaslpge@users.noreply.github.com>
2022-10-18 15:11:10 +0200

    Add files via upload

---
+++ Trafic.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import altair as alt
+import pydeck as pdk
+from streamlit_option_menu import option_menu
+
+
+st.markdown("#Trafic")
+st.sidebar.markdown("Trafic")
+
+def home():
+    st.markdown("#Home")
+    st.sidebar.markdown("#Home")
+
+def page2():
+    st.markdown("#Trafic")
+    st.sidebar.markdown("#Trafic")
+
+def page3():
+    st.markdown("#Route Planning")
+    st.sidebar.markdown("#Route Planning")
+
+df = pd.read_csv("clean_taxi.csv")
+
+st.header("Take a look at the Trafic")
+st.subheader("Just say us what day and what time you want to check the trafic :")
+day_to_filter = st.slider('Day', 2, 8, 2)
+hour_to_filter = st.slider('Hour', 0, 23, 15)
+minute_to_filter = st.slider('Minute', 0, 59, 30)
+filtered_data = df.loc[(df.hour == hour_to_filter) & (df.day == day_to_filter) & (df.minute == minute_to_filter)]
+st.map(filtered_data)
