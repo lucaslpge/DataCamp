e3ea03396310571559573ab400a73b590fb16d4d
lucaslpge <lucas.le.page@efrei.net>
2022-10-19 14:03:22 +0200

    update

--- pages/Trafic.py
+++ pages/Trafic.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 st.markdown("#Trafic")
 st.sidebar.markdown("Trafic")
 
@@ -19,21 +21,26 @@ def page3():
 
 def map(df):
     st.header("Take a look at the Trafic")
-    st.subheader("Just say us what day and what time you want to check the trafic :")
-    day_to_filter = st.slider('Day', 2, 8, 2)
-    hour_to_filter = st.slider('Hour', 0, 23, 15)
-    start_minute, end_minute = st.select_slider(
-        'Minute',
-        options=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 59],
-        value=(15, 30))
-
-    st.write('You are looking at the trafic at the ', day_to_filter, '/02/2008 between', hour_to_filter, ':', start_minute, 'and', hour_to_filter, ':', end_minute)
-    filtered_data = df.loc[(df.hour == hour_to_filter) & (df.day == day_to_filter) & (df.minute >= start_minute) & (df.minute <= end_minute)]
-    st.map(filtered_data)
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.subheader("Just say us what day and what time you want to check the trafic :")
+        day_to_filter = st.slider('Day', 2, 8, 2)
+        hour_to_filter = st.slider('Hour', 0, 23, 15)
+        start_minute, end_minute = st.select_slider(
+            'Minute',
+            options=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 59],
+            value=(15, 30))
+    with right_col:
+        st.write('You are looking at the trafic at the ', day_to_filter, '/02/2008 between', hour_to_filter, ':', start_minute, 'and', hour_to_filter, ':', end_minute)
+        filtered_data = df.loc[(df.hour == hour_to_filter) & (df.day == day_to_filter) & (df.minute >= start_minute) & (df.minute <= end_minute)]
+        st.map(filtered_data)
 
 def main(df):
     map(df)
 
+
+st.header('Welcome to the Traffic page !')
+st.subheader('Please import a csv :')
 df = st.file_uploader("Choose a file")
 if df is not None:
     df = pd.read_csv(df)
